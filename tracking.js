feea3bffa5daaee2f8a42ec77d238195b5de10fe
aust-ase2 <101862047+aust-ase2@users.noreply.github.com>
2022-12-21 22:28:25 +0700

    Update tracking.js

--- tracking.js
+++ tracking.js
@@ -292,32 +292,24 @@
     }
 
 
-function createXPathFromElement(elm) { 
-    var allNodes = document.getElementsByTagName('*'); 
-    for (var segs = []; elm && elm.nodeType == 1; elm = elm.parentNode) 
-    { 
-        if (elm.hasAttribute('id')) { 
-                var uniqueIdCount = 0; 
-                for (var n=0;n < allNodes.length;n++) { 
-                    if (allNodes[n].hasAttribute('id') && allNodes[n].id == elm.id) uniqueIdCount++; 
-                    if (uniqueIdCount > 1) break; 
-                }; 
-                if ( uniqueIdCount == 1) { 
-                    segs.unshift('id("' + elm.getAttribute('id') + '")'); 
-                    return segs.join('/'); 
-                } else { 
-                    segs.unshift(elm.localName.toLowerCase() + '[@id="' + elm.getAttribute('id') + '"]'); 
-                } 
-        } else if (elm.hasAttribute('class')) { 
-            segs.unshift(elm.localName.toLowerCase() + '[@class="' + elm.getAttribute('class') + '"]'); 
-        } else { 
-            for (i = 1, sib = elm.previousSibling; sib; sib = sib.previousSibling) { 
-                if (sib.localName == elm.localName)  i++; }; 
-                segs.unshift(elm.localName.toLowerCase() + '[' + i + ']'); 
-        }; 
-    }; 
-    return segs.length ? '/' + segs.join('/') : null; 
-}; 
+function createXPathFromElement(element) { 
+  
+    if (element.tagName == 'HTML')
+        return '/HTML[1]';
+    if (element===document.body)
+        return '/HTML[1]/BODY[1]';
+
+    var ix= 0;
+    var siblings= element.parentNode.childNodes;
+    for (var i= 0; i<siblings.length; i++) {
+        var sibling= siblings[i];
+        if (sibling===element)
+            return getPathTo(element.parentNode)+'/'+element.tagName+'['+(ix+1)+']';
+        if (sibling.nodeType===1 && sibling.tagName===element.tagName)
+            ix++;
+    }
+
+}
 
 
 
